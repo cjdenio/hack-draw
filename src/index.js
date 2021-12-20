19842e442a19308cdb165c22804f5c3b3696dac2
Caleb Denio <cjdenio44@gmail.com>
2021-12-20 01:22:30 -0500

    CORS

--- src/index.js
+++ src/index.js
@@ -21,7 +21,12 @@ const app = express();
 
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5000", "https://hackclub.github.io"],
+    methods: ["GET", "POST"],
+  },
+});
 const drawNamespace = io.of("/draw");
 
 const wrap = (middleware) => (socket, next) =>
